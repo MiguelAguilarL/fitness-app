36dc8ec8ba451e3dbf571ae86157f1415ec9b5bc
Miguel <mikeal2123495@outlook.com>
2026-05-06 11:51:47 -0600

    Minor changes

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-700">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Fitness app!
       </Text>
     </View>
   );
---
+++ components/Set.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native-web";
+
+export default function Set() {
+  return (
+    <View>
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  )
+}

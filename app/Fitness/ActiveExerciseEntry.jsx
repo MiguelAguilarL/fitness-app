7fb8b58fcffaa335e8551532babb9335bfbe435b
alexsol <solisalexander12006@gmail.com>
2026-05-16 22:51:20 -0600

    Y más cambios.

--- app/Fitness/ActiveExerciseEntry.jsx
+++ app/Fitness/ActiveExerciseEntry.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { StatusBar, View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import WorkoutCard from '../../components/Home/WorkoutCard'
 
 export default function ActiveExerciseEntry() {
+  const navigation = useNavigation()
+
   return (
     <SafeAreaView edges={['top','bottom']} className="flex-1 bg-[#07060a]">
       <StatusBar barStyle="light-content" backgroundColor="#07060a" />
@@ -11,7 +14,7 @@ export default function ActiveExerciseEntry() {
       <View className="px-4 pt-6">
         <Text className="text-[#d6a6ff] text-[26px] font-extrabold">Sesión de hoy</Text>
         <View className="h-3" />
-        <WorkoutCard />
+        <WorkoutCard onCardPress={() => navigation.navigate('RoutineDetail', { routineId: 'back-biceps' })} />
       </View>
     </SafeAreaView>
   )
